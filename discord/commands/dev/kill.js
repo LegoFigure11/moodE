89b16185fea3bd76e5540762828abbf28c53a8dd
LegoFigure11 <legofigurefb@gmail.com>
2020-04-14 09:33:11 +1000

    Update discord kill command

--- discord/commands/dev/kill.js
+++ discord/commands/dev/kill.js
@@ -4,7 +4,7 @@ module.exports = {
 	desc: "Ends the current bot process.",
 	adminOnly: true,
 	async process(message, args) {
-		message.channel.send("Ending process...");
+		await message.channel.send("Goodbye!");
 		console.log(`${discordText}${"Ending all bot processes...".brightRed}`);
 		console.log(`${discordText}${"This can now be safely closed!".green}`);
 		eval("process.exit(0);");
