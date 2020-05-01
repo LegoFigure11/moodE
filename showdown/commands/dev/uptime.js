a5b0f2cf7c8ce6ddcd69f7ca2ccced715f16a7ca
LegoFigure11 <legofigurefb@gmail.com>
2020-05-01 22:46:05 +1000

    Fix typo causing crash

--- showdown/commands/dev/uptime.js
+++ showdown/commands/dev/uptime.js
@@ -6,7 +6,7 @@ module.exports = {
 	desc: "Prints information about how long the bot has been running.",
 	async process(args, room, user) {
 		let text = "";
-		if (!room.isPm() && !user.hasRank(room, "@") && !user.isDeveloper()) text = `/pm ${user.id}, `;
+		if (!room.isPm() && !user.hasRoomRank(room, "@") && !user.isDeveloper()) text = `/pm ${user.id}, `;
 		text += "**Uptime:** ";
 		const divisors = [52, 7, 24, 60, 60];
 		const units = ["week", "day", "hour", "minute", "second"];
