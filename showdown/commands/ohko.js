05e8d660f24eda335d338f81e1b48ef8984ca19c
LegoFigure11 <legofigurefb@gmail.com>
2020-05-01 22:41:20 +1000

    Make OHKO command actually 30% `Tools.random(n)` returns 0-n, not 1-n

--- showdown/commands/ohko.js
+++ showdown/commands/ohko.js
@@ -4,6 +4,6 @@ module.exports = {
 	desc: "Uses a One-hit KO move.",
 	roomRank: "+",
 	async process(args, room, user) {
-		return room.say(Tools.random(10) <= 3 ? "It's a one-hit KO!" : "The attack missed!");
+		return room.say(Tools.random(10) < 3 ? "It's a one-hit KO!" : "The attack missed!");
 	},
 };
