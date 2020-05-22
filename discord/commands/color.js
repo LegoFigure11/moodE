77895a82dca42a3405abf91d868d52d4c3260a21
LegoFigure11 <legofigurefb@gmail.com>
2020-05-22 11:30:57 +1000

    Discord: Upgrade color command do display on more backgrounds

--- discord/commands/color.js
+++ discord/commands/color.js
@@ -13,22 +13,53 @@ module.exports = {
 			color = color.trim().substr(0, 8);
 		}
 		color = color.replace("0x", "#");
+		if (!color.startsWith("#")) color = `#${color}`;
 		if (!(hexRegex.test(color.trim()))) return message.channel.send(`${discordConfig.failureEmoji} Unable to coerce "${args[0]}" as a hex code!`);
 		const image = await nodeHtmlToImage({
 			html: `
-			<html>
-				<head>
-					<style>
-						body {
-							width: 100px;
-							height: 100px;
-							background-color: ${hexRegex.exec(color)[0]};
-						}
-					</style>
-				</head>
-				<body>
-				</body>
-			</html>`,
+<html>
+	<head>
+		<style>
+			body {
+				font-family: Arial, sans-serif;
+				width: 300px;
+				height: 100px;
+				display: flex;
+				flex-direction: row;
+				text-align: center;
+				font-size: 16px;
+				font-weight: 500;
+			}
+			div {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+			.color {
+				width: 100px;
+				height: 100px;
+				background-color: ${hexRegex.exec(color)[0]};
+			}
+			.dark {
+				width: 100px;
+				height: 100px;
+				background-color: #36393F;
+				color: ${hexRegex.exec(color)[0]};
+			}
+			.light {
+				width: 100px;
+				height: 100px;
+				background-color: #FFFFFF;
+				color: ${hexRegex.exec(color)[0]};
+			}
+		</style>
+	</head>
+	<body>
+		<div class="color"></div>
+		<div class="dark"><b>Dark Mode</b></div>
+		<div class="light"><b>Light Mode</b></div>
+	</body>
+</html>`,
 			puppeteerArgs: ["--no-sandbox", "--disable-setuid-sandbox"],
 		});
 
