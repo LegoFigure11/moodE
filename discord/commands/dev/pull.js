26ccaa5c0cc78e6a5d38e67e7db412a211547eb8
LegoFigure11 <legofigurefb@gmail.com>
2020-05-29 22:02:01 +1000

    Discord: Fix crash in pull command

--- discord/commands/dev/pull.js
+++ discord/commands/dev/pull.js
@@ -16,21 +16,21 @@ module.exports = {
 
 		const remoteOutput = await exec("git remote -v").catch(e => console.log(e));
 		if (!remoteOutput || remoteOutput.Error) {
-			console.log(`${moodeText}${"Error".red}: No git remote output.`);
+			console.log(`${Tools.moodeText()}${"Error".red}: No git remote output.`);
 			return message.channel.send(`${failureEmoji} No git remote output.`);
 		}
 
-		console.log(`${moodeText}Attempting pull...`);
+		console.log(`${Tools.moodeText()}Attempting pull...`);
 		const pull = await exec("git pull");
 		if (!pull || pull.Error) {
 			console.log(`${moodeText}Error: could not pull origin.`);
 			return message.channel.send(`${failureEmoji} Error: could not pull origin.`);
 		}
 		if (pull.stdout.replace("\n", "").replace(/-/g, " ") === "Already up to date.") {
-			console.log(`${moodeText}Already up to date!`);
+			console.log(`${Tools.moodeText()}Already up to date!`);
 			return message.channel.send(`${failureEmoji} Already up to date!`);
 		} else {
-			console.log(`${moodeText}Pull completed!`);
+			console.log(`${Tools.moodeText()}Pull completed!`);
 			return message.channel.send(`${successEmoji} Pull completed!`);
 		}
 	},
