e484c8482e57a3baad588fc450b61e028cdafabc
LegoFigure11 <legofigurefb@gmail.com>
2020-05-31 15:46:03 +1000

    Discord: Add SID for Shiny command

---
+++ discord/commands/rng/sidforshiny.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+	desc: ".",
+	usage: "<PID (Hex)>, <TID (Dec)>",
+	aliases: ["sid4shiny", "square"],
+	async process(message, args) {
+		const decPid = parseInt(args[0], 16);
+		const decTid = parseInt(args[1]);
+
+		if (isNaN(decPid)) return message.channel.send(`${discordConfig.failureEmoji} Unable to coerce ${args[0]} as a Hex string!`);
+		if (isNaN(decTid)) return message.channel.send(`${discordConfig.failureEmoji} Unable to coerce ${args[1]} as a decimal!`);
+		if (decPid < 0 || decPid > 0xFFFFFFFF) return message.channel.send(`${discordConfig.failureEmoji} ${args[0]} is not a valid PID!`);
+		if (decTid < 0 || decTid > 65535) return message.channel.send(`${discordConfig.failureEmoji} ${args[1]} is not a valid TID!`);
+
+		const pid16High = decPid >>> 16;
+		const pid16Low = decPid & 0xFFFF;
+
+		const square = pid16High ^ pid16Low ^ decTid;
+		const rangeLow = 8 * Math.floor(square / 8);
+
+		return message.channel.send(`SID range for shiny: ${rangeLow} - ${rangeLow + 8}\nSID to force Square Shiny: ${square}`);
+	},
+};
