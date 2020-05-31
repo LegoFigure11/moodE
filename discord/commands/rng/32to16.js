f7013ff245acebbee04982df8de5b124977db029
LegoFigure11 <legofigurefb@gmail.com>
2020-05-31 15:47:09 +1000

    Discord: Clean up 32to16 command

--- discord/commands/rng/32to16.js
+++ discord/commands/rng/32to16.js
@@ -3,8 +3,10 @@
 const LCRNG = require("../../../sources/rng/lcrng.js");
 
 module.exports = {
-	desc: "Gets the nearest 16-bit seed to a provided 32-bit seed.",
+	desc: "Gets the nearest 16-bit seed to a provided 32-bit seed. Uses standard GBA LCRNG.",
+	usage: "<32-bit seed (as Hex)>",
 	async process(message, args) {
+		if (isNaN(parseInt(args[0], 16))) return message.channel.send(`Unable to coerce ${args[0]} as a Hex string!`);
 		const RNG = new LCRNG.PokeRNGR(args[0]);
 		let result;
 		let i = 0;
