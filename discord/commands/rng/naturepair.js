f4e7e4f2d407b4ca37fae6e04c75d809d0f6f177
LegoFigure11 <legofigurefb@gmail.com>
2020-08-02 18:03:24 +1000

    RNG: PID to Seed

---
+++ discord/commands/rng/naturepair.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const LCRNG = require("../../../sources/rng/lcrng.js");
+const RNG = new LCRNG.PokeRNG();
+const RNG2 = new LCRNG.XDRNG();
+
+module.exports = {
+	desc: "Provides the Nature Pair to a given PID",
+	usage: "<PID (Hex)>",
+	aliases: ["pair"],
+	async process(message, args) {
+		if (!args[0]) return message.channel.send(this.usage);
+		const pid1 = args[0].toString(16);
+		const decPid1 = parseInt(pid1, 16);
+
+		if (isNaN(decPid1)) return message.channel.send(`${discordFailureEmoji} Unable to coerce ${args[0]} as a Hex string!`);
+		if (decPid1 < 0 || decPid1 > 0xFFFFFFFF) return message.channel.send(`${discordFailureEmoji} ${args[0]} is not a valid PID!`);
+
+		const method124Seeds = RNG.calcMethod124SeedIVs(decPid1);
+		let m1pids = [];
+		for (let i = 0; i < method124Seeds.length; i++) {
+			m1pids.push(RNG.concat16([RNG.getNext16BitNumber(RNG.getNext32BitNumber(method124Seeds[i][0])), RNG.getNext16BitNumber(method124Seeds[i][0])]));
+			method124Seeds[i][0] = (parseInt(method124Seeds[i][0], 16) + 0x80000000) & 0xFFFFFFFF;
+			m1pids.push(RNG.concat16([RNG.getNext16BitNumber(RNG.getNext32BitNumber(method124Seeds[i][0])), RNG.getNext16BitNumber(method124Seeds[i][0])]));
+		}
+
+		const methodXDSeeds = RNG2.calcMethodXDSeedIVs(decPid1);
+		let xdpids = [];
+		for (let i = 0; i < methodXDSeeds.length; i++) {
+			xdpids.push(RNG2.concat16([RNG2.getNext16BitNumber(RNG2.getNext32BitNumber(methodXDSeeds[i][0], 3)), RNG2.getNext16BitNumber(RNG2.getNext32BitNumber(methodXDSeeds[i][0], 4))]));
+			methodXDSeeds[i][0] = (parseInt(methodXDSeeds[i][0], 16) + 0x80000000) & 0xFFFFFFFF;
+			xdpids.push(RNG2.concat16([RNG2.getNext16BitNumber(RNG2.getNext32BitNumber(methodXDSeeds[i][0], 3)), RNG2.getNext16BitNumber(RNG2.getNext32BitNumber(methodXDSeeds[i][0], 4))]));
+		}
+
+		m1pids = [...new Set(m1pids)];
+		xdpids = [...new Set(xdpids)];
+
+		const m1natures = [];
+		for (let i = 0; i < m1pids.length; i += 2) {
+			m1natures.push(`\n\t• ${RNG.natures[m1pids[i] % 25]} / ${RNG.natures[m1pids[i + 1] % 25]} (${m1pids[i]} / ${m1pids[i + 1]})`);
+		}
+
+		const xdnatures = [];
+		for (let i = 0; i < xdpids.length; i += 2) {
+			xdnatures.push(`\n\t• ${RNG.natures[xdpids[i] % 25]} / ${RNG.natures[xdpids[i + 1] % 25]} (${xdpids[i]} / ${xdpids[i + 1]})`);
+		}
+
+		return message.channel.send(`\`\`\`Nature Pairs for PID ${args[0]}:\n${m1pids.length > 0 ? `Method 1/2/4:${m1natures.join("")}` : ""}\n${xdpids.length > 0 ? `Colo/XD:${xdnatures.join("")}` : ""}\`\`\``);
+	},
+};
--- sources/rng/lcrng.js
+++ sources/rng/lcrng.js
@@ -2,11 +2,33 @@
 
 const converter = require("hex2dec");
 
+const natures = [
+	"Hardy", "Lonely", "Brave", "Adamant", "Naughty",
+	"Bold", "Docile", "Relaxed", "Impish", "Lax",
+	"Timid", "Hasty", "Serious", "Jolly", "Naive",
+	"Modest", "Mild", "Quiet", "Bashful", "Rash",
+	"Calm", "Gentle", "Sassy", "Careful", "Quirky",
+];
+
 class LCRNG {
 	constructor(seed, add, mult) {
 		this.seed = seed;
 		this.add = add;
 		this.mult = mult;
+		this.natures = natures;
+		this.right = 0;
+		this.val = 0;
+		this.flags = [];
+		this.low8 = [];
+		for (let i = 0; i < 256; i++) {
+			this.right = converter.decToHex(`${this.unsign(0x41c64e6d * i + 0x6073)}`);
+			this.val = this.right >>> 16;
+			this.flags[this.val] = true;
+			this.low8[this.val] = i;
+			this.val--;
+			this.flags[this.val] = true;
+			this.low8[this.val] = i;
+		}
 	}
 
 	getNext32BitNumber(seed = this.seed, times = 1) {
@@ -23,6 +45,66 @@ class LCRNG {
 		return converter.decToHex(`${this.getNext32BitNumber(seed) >>> 16}`);
 	}
 
+	calcMethod124SeedIVs(pid) {
+		const ret = [];
+		const pidl = this.unsign((pid & 0xFFFF) << 16);
+		const pidh = pid & 0xFFFF0000;
+
+		let k1 = this.unsign(pidh - pidl * 0x41c64e6d);
+		for (let cnt = 0; cnt < 256; ++cnt, k1 = this.unsign(k1 - 0xc64e6d00)) {
+			const test = k1 >>> 16;
+			if (this.flags[test]) {
+				const fullFirst = this.unsign(pidl | (cnt << 8) | this.low8[test]);
+			  const fullSecond = this.getNext32BitNumber(fullFirst);
+			  if ((fullSecond & 0xFFFF0000) === pidh) {
+					ret.push([`${converter.decToHex(`${this.reverse(fullFirst)}`)}`, `${this.getNext32BitNumber(fullSecond)}`]);
+				}
+		  }
+		}
+		return ret;
+	}
+
+	calcMethodXDSeedIVs(pid) {
+		const ret = [];
+		const first = pid & 0xFFFF0000;
+		const second = (pid & 0xFFFF) << 16;
+		let fullFirst;
+
+		let t = ((second - 0x343fd * first) - 0x259ec4) & 0xFFFFFFFF;
+		const kmax = (0x343fabc02 - t) / 0x100000000;
+
+		for (let k = 0; k <= kmax; k++, t += 0x100000000) {
+			if ((t % 0x343fd) < 0x10000) {
+				fullFirst = this.unsign(first | (t / 0x343fd));
+				const iv2 = this.reverseXD(this.reverseXD(fullFirst));
+				const iv1 = this.reverseXD(iv2);
+				ret.push([`${converter.decToHex(`${this.reverseXD(iv1)}`)}`, iv1, iv2, 0]);
+			}
+		}
+		return ret;
+	}
+
+	reverse(seed) {
+		seed = this.unsign(Math.imul(seed, 0xeeb9eb65));
+		seed = this.unsign(seed + 0xa3561a1);
+		seed = this.unsign(seed & 0xFFFFFFFF);
+		return seed;
+	}
+
+	reverseXD(seed) {
+		seed = this.unsign(Math.imul(seed, 0xB9B33155));
+		seed = this.unsign(seed + 0xA170F641);
+		seed = this.unsign(seed & 0xFFFFFFFF);
+		return seed;
+	}
+
+	concat16(args) {
+		let ret = "0x";
+		for (let i = 0; i < args.length; i++) {
+			ret += args[i].replace("0x", "").padStart(4, "0");
+		}
+		return ret;
+	}
 	// From X-Act IVs to PID applet
 	unsign(int) {
 		return (int >>> 1) * 2 + (int & 1);
