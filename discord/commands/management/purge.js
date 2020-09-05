474b0d013b1cf7df91383a9734a34f925457a7ab
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2020-09-05 21:46:35 +1000

    Discord: Fix multiple issues with purge command

--- discord/commands/management/purge.js
+++ discord/commands/management/purge.js
@@ -6,7 +6,7 @@ const TIMEOUT = 30 * 1000;
 
 module.exports = {
 	desc: "Removes many messages from a channel.",
-	longDesc: "Removes the last 1 or more messages in a channel. If an author is provided, only delete messages by that user.",
+	longDesc: "Removes up to the last 1 or more messages in a channel. If an author is provided, only delete messages by that user.\nOnly the most recent 100 messages can be accessed at once, so the number of actual deleted messages may be lower than the provided number.\nMessages older than two weeks cannot be deleted in this manner.",
 	usage: "<number (optional, default 1, max 100)>, <@author|author id (optional, default none)>",
 	aliases: ["delete", "wipe", "bulkdelete", "deletemany"],
 	isManager: true,
@@ -25,30 +25,35 @@ module.exports = {
 				if (!isNaN(tempNum)) num = tempNum;
 			}
 		}
+
+		num = Math.min(num, 100);
 		// Get confirmation before proceeding
 		const filter = response => {
 			return response.author.id === message.author.id && response.channel.id === message.channel.id && (Tools.toId(response.content) === "y" || Tools.toId(response.content) === "n");
 		};
-
-		message.channel.send(`Are you sure you want to delete the last ${num === 1 ? "" : num} message${num === 1 ? "" : "s"} ${users.length > 0 ? `by ${users[0]}` : ""}? (Y/n):`).then(() => {
-			message.channel.awaitMessages(filter, {max: 1, time: TIMEOUT, errors: ["time"]}).then(async (collected) => {
-				if (Tools.toId(collected.first().content) === "y") {
-					let messages = await message.channel.messages.fetch({limit: num + 50}).catch(e => { return message.channel.send(`${discordFailureEmoji} Error fetching messages!`); });
-					if (users[0]) {
-						messages = messages.filter(m => m.author.id === users[0].id).array().slice(0, num);
+		try {
+			message.channel.send(`Are you sure you want to delete the last ${num === 1 ? "" : num} message${num === 1 ? "" : "s"} ${users.length > 0 ? `by ${users[0]}` : ""}? (Y/n):`).then(() => {
+				message.channel.awaitMessages(filter, {max: 1, time: TIMEOUT, errors: ["time"]}).then(async (collected) => {
+					if (Tools.toId(collected.first().content) === "y") {
+						let messages = await message.channel.messages.fetch({limit: 100}).catch(e => { console.log(e); return message.channel.send(`${discordFailureEmoji} Error fetching messages!`); });
+						if (users[0]) {
+							messages = messages.filter(m => m.author.id === users[0].id).array().slice(0, num);
+						}
 						try {
-							message.channel.bulkDelete(messages);
-							message.channel.send(`${discordSuccessEmoji} ${messages.length} message${message.length === 1 ? "" : "s"} were deleted!`);
+							const deleted = await message.channel.bulkDelete(messages, true);
+							message.channel.send(`${discordSuccessEmoji} ${deleted.size} message${deleted.size === 1 ? "" : "s"} were deleted!`);
 						} catch (e) {
 							console.log(e.stack);
 						}
+					} else {
+						message.channel.send("No messages will be deleted!");
 					}
-				} else {
-					message.channel.send("No messages will be deleted!");
-				}
-			}).catch(collected => {
-				return message.channel.send("The command timed out! No messages will be deleted.");
+				}).catch(collected => {
+					return message.channel.send("The command timed out! No messages will be deleted.");
+				});
 			});
-		});
+		} catch (e) {
+			console.log(`${Tools.discordText()}Something went wrong...`);
+		}
 	},
 };
