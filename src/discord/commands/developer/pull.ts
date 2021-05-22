f860ca1b79eb71ed17f1dc0eb98c96f4582e9b99
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2021-05-22 22:40:46 +1000

    Add pull command

---
+++ src/discord/commands/developer/pull.ts
@@ -0,0 +1,30 @@
+import {Permissions} from "discord.js";
+import {UserPermissions} from "../../enums/userPermissions";
+import type {ICommand} from "../../../types/commands";
+
+import {exec} from "child_process";
+import {promisify} from "util";
+const sh = promisify(exec);
+
+module.exports = {
+  desc: "Attempts to execute git pull.",
+  commandPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+  userPermissons: UserPermissions.DEVELOPER,
+  async command(message) {
+    const msg = await message.channel.send("Attempting git pull...");
+    const remoteOutput = await sh("git remote -v").catch(console.error);
+    if (!remoteOutput || remoteOutput.stderr) {
+      return msg.edit(Utilities.failureEmoji(message, "No git remote output."));
+    }
+
+    const pull = await sh("git pull").catch(console.error);
+    if (!pull || pull.stderr) {
+      return msg.edit(Utilities.failureEmoji(message, "Could not pull origin."));
+    }
+    if (pull.stdout.replace("\n", "").replace(/-/g, " ") === "Already up to date.") {
+      return msg.edit(Utilities.failureEmoji(message, "Already up to date!"));
+    } else {
+      return msg.edit(Utilities.successEmoji(message, `Pull completed!\`\`\`${pull.stdout}\`\`\``));
+    }
+  },
+} as ICommand;
