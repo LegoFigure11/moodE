2b63b1b92bf8e212c7a6be32413fef841248d07c
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2021-06-13 03:47:39 +1000

    Fix starboard pt 4

--- discord/reactionEvents/remove/starboard.js
+++ discord/reactionEvents/remove/starboard.js
@@ -28,7 +28,7 @@ module.exports = {
 				if (reaction.count >= db.starboard.requiredStars) {
 					const channel = client.channels.cache.get(db.starboard.channel);
 					channel.messages.fetch(db.starboard.stars[reaction.message.id]).then(msg => {
-						let embed = {};
+						let embed = null;
 						if (msg.embeds && msg.embeds[0]) embed = msg.embeds[0];
 						msg.edit(`${reaction.count <= 5 ? (db.starboard.level1 ? db.starboard.level1 : ":star:") : reaction.count <= 10 ? (db.starboard.level2 ? db.starboard.level2 : ":star2:") : (db.starboard.level2 ? db.starboard.level3 : ":stars:")} **${reaction.count}** - ${reaction.message.channel} (${reaction.message.author})`, {embed});
 					});
