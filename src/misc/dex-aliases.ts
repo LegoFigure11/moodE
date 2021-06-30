e2f42031c1a9da964b9fdef8c53b3b128ec74920
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2021-06-30 22:05:17 +1000

    Update dex aliases

--- src/misc/dex-aliases.ts
+++ src/misc/dex-aliases.ts
@@ -1,8 +1,18 @@
 export const Aliases: KeyedDict<string, string> = {
+  // Typos
+  "delibirb": "delibird",
+
   // Cool people
+  "archie": "jirachi",
+  "lego": "eldegoss",
   "mica": "shaymin",
+  "onion": "eldegoss",
   "psychicj": "gardevoir",
+  "saiph": "gengar",
   "xen": "qwilfish",
+
+  // Dex Numbers
+  "360": "wynaut",
 };
 
 export function getAlias(args: string | undefined): string {
