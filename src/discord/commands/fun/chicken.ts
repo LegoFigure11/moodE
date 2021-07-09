6fd74c9fecf1b71539c092a09627e7ed012b2e53
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2021-07-09 23:28:06 +1000

    Add Chicken command

---
+++ src/discord/commands/fun/chicken.ts
@@ -0,0 +1,24 @@
+import {Permissions} from "discord.js";
+import type {ICommand} from "../../../types/commands";
+
+const chickens = [
+  "\u{1F414}", // :chicken:
+  "\u{1F424}", // :baby_chick:
+  "\u{1F423}", // :hatching_chick:
+  "\u{1F425}", // :hatched_chick:
+  "\u{1F413}", // :rooster:
+  "\u{1F357}", // :poultry_leg:
+  "\u{1F95A}", // :egg:
+  "\u{1F373}", // :cooking:
+];
+
+module.exports = {
+  desc: "Cluck cluck. Cluck cluck?",
+  commandPermissions: [Permissions.FLAGS.MANAGE_NICKNAMES],
+  command(message) {
+    const nick = message.member!.nickname || message.author.username;
+    const chook = Utilities.sampleOne(chickens);
+    message.member!.setNickname(nick + chook).catch(console.error);
+    return message.channel.send(`Did somebody say... CHICKEN!?!? ${chook}`).catch(console.error);
+  },
+} as ICommand;
