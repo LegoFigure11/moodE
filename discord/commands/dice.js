066482390c379dd5ca7c54aa0f98366d2327eb6f
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2021-07-18 21:44:39 +1000

    Add dice aliases

--- discord/commands/dice.js
+++ discord/commands/dice.js
@@ -13,7 +13,7 @@ module.exports = {
 		`${discordConfig.commandCharacter}dice <number of dice>d<number of sides><+/-><offset> - Simulates rolling a number of dice and adding an offset to the sum, e.g., ${discordConfig.commandCharacter}dice 2d6+10: two standard dice are rolled; the result lies between 12 and 22.`,
 		`${discordConfig.commandCharacter}dice <number of dice>d<number of sides>-<H/L> - Simulates rolling a number of dice with removal of extreme values, e.g., /dice 3d8-L: rolls three 8-sided dice; the result ignores the lowest value.`],
 	usage: "<number of sides>",
-	aliases: ["roll"],
+	aliases: ["roll", "d", "r"],
 	async process(message, args) {
 		let target = args.join("");
 
