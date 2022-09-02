3dd7c4ff31b4c44ebc6f05925f0b1aae7e2a9af6
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-09-03 09:52:53 +1000

    Lint unown command

--- src/discord/commands/dex/unown.ts
+++ src/discord/commands/dex/unown.ts
@@ -3,17 +3,23 @@ import type {ICommand} from "../../../types/commands";
 
 const alphabet = [
   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-  "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?"
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?",
 ];
 
 module.exports = {
   desc: "Translates an Unown form into its corresponding letter.",
   commandPermissions: [Permissions.FLAGS.SEND_MESSAGES],
   usage: "<Unown Number>",
-  async command(message, args) {
-    let num = parseInt(args?.[0]);
+  command(message, args) {
+    const num = parseInt(args?.[0]);
     if (num && !isNaN(num)) {
-      message.channel.send(alphabet[num % alphabet.length]);
+      return message.reply(
+        {
+          content:
+            alphabet[num % alphabet.length],
+          allowedMentions: {repliedUser: false},
+        }
+      );
     }
   },
 } as ICommand;
