206894d4519bc5c3998886017b1939fc947b8c68
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-09-03 09:49:06 +1000

    Add pet command Now swf will be happy

---
+++ src/discord/commands/fun/pet.ts
@@ -0,0 +1,16 @@
+import {Permissions} from "discord.js";
+import type {ICommand} from "../../../types/commands";
+
+module.exports = {
+  desc: "/me pet",
+  commandPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+  command(message, args) {
+    return message.reply(
+      {
+        content:
+          `_/me pet ${args[0] ? args[0] : ""}_`,
+        allowedMentions: {repliedUser: false},
+      }
+    );
+  },
+} as ICommand;
